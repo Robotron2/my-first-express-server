eaab12938781858ab373da3387f0ce8c388bc2c3
Theophilus Adeleke <theophilusadesola002@gmail.com>
2023-01-03 13:03:37 +0100

    Created a get requent and sent a response

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const express = require("express")
 // Initialize express
 const app = express()
 
+app.get("/", (request, response) => {
+	response.send("Hello World")
+})
+
 //Listen on a port
 app.listen(3000, () => {
 	console.log("App is running on port 3000")
