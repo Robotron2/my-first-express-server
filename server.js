0d0c3c7c37c2ff537e07725b03ff84193d587039
Theophilus Adeleke <theophilusadesola002@gmail.com>
2023-01-03 13:13:28 +0100

    Created contact and about routes

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ app.get("/", (request, response) => {
 	response.send("Hello World")
 })
 
+app.get("/contact", (req, res) => {
+	res.send("Contact me: theo@gmail.com")
+})
+
+app.get("/about", (req, res) => {
+	res.send("I am fullstack web developer, currently in training.")
+})
+
 //Listen on a port
 app.listen(3000, () => {
 	console.log("App is running on port 3000")
